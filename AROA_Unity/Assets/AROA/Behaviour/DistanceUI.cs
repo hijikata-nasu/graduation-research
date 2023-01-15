443031674a8894dafa01c608c39b328d2efe2fb2
hijikata <kaitomikkumiku+github@gmail.com>
2023-01-16 01:10:10 +0900

    DistanceUIにスクリプトを追加した
    外部からセットされた値により円のスケールを操作して距離を表すようにした

---
+++ AROA_Unity/Assets/AROA/Behaviour/DistanceUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AROA.Behaviour
+{
+    public class DistanceUI : MonoBehaviour
+    {
+        [SerializeField] private Transform targetCircle;
+        [SerializeField] private Transform zeroCircle;
+        [SerializeField] private float zeroScale = 100;
+        private float _distance = 100;
+
+        public float Distance
+        {
+            get => _distance;
+            set
+            {
+                if (Distance < 0)
+                {
+                    _distance = 0;
+                    return;
+                }
+                _distance = value;
+            }
+        }
+
+        void Update()
+        {
+            zeroCircle.localScale = new Vector3(zeroScale, zeroScale, zeroCircle.localScale.z);
+            targetCircle.localScale = new Vector3(zeroScale + _distance, zeroScale + _distance, targetCircle.localScale.z);
+        }
+    }
+}
